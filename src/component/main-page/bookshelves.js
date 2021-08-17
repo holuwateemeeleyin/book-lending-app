b1af6488d5158557647e533b11f3485a3251ffa7
samuel abegunde <abegundeosamuel@gmail.com>
2021-08-17 15:48:38 +0100

    style: improve readability

--- src/component/main-page/bookshelves.js
+++ src/component/main-page/bookshelves.js
@@ -4,17 +4,17 @@ import BookshelfBooks from './bookshelfBooks';
 const BookShelves = (props) => {
     const { shelf, books, onChangeBook } = props
     return (
-            <div className="bookshelf">
-                {/* display Each Shelf category heading */}
-                <h2 className="bookshelf-title"> {shelf.category}</h2>
-                <div className="bookshelf-books">
+        <div className="bookshelf">
+            {/* display Each Shelf category heading */}
+            <h2 className="bookshelf-title"> {shelf.category}</h2>
+            <div className="bookshelf-books">
                 <ol className="books-grid">
                     {/* Comparing the book.shelf(in our API) and shelf.id(that was stored in array of objects as shelves in app.js files),
                         if it matches, then display Books on their categeory
                     */}
-                {books.filter(book => (             
+                    {books.filter(book => (
                         book.shelf === shelf.id         // comaparing book.shelf in our API and shelf.id in shelves
-                    )).map((book)=> (       //if it matches, then display the books imperatively
+                    )).map((book) => (       //if it matches, then display the books declaratively
                         <BookshelfBooks
                             key={book.id}
                             book={book}
@@ -23,11 +23,8 @@ const BookShelves = (props) => {
                         />
                     ))}
                 </ol>
-                </div>
-                {/* <BookshelfBooks books={books} 
-                shelf={shelf} 
-                onChangeBook={onChangeBook}/> */}
             </div>
+        </div>
     )
 }
 export default BookShelves;
